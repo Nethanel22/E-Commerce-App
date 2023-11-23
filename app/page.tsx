8629d304714a7e034d1b0845d6a9802100d91c49
Nethanel <nethanel676@gmail.com>
2023-11-23 16:48:31 +0200

    intail HomeBanner1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
 
 export default function Home() {
   return (
-    <main>E-Shop</main>
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner/>
+        </div>
+      </Container>
+    </div>
   )
 }
