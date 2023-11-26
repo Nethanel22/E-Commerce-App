32459060bf6ad585d0f5b69c23f75c97dd97f4e5
Nethanel <nethanel676@gmail.com>
2023-11-26 16:03:15 +0200

    productDb completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { products } from "@/utils/Products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
-
+import { truncateText } from "@/utils/truncateText";
 export default function Home() {
   return (
     <div className="p-8">
@@ -9,10 +9,10 @@ export default function Home() {
         <div>
           <HomeBanner/>
         </div>
-        <div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
           {products.map((product:any) =>{
             return <div>
-              {product.name}
+              {truncateText(product.name)}
             </div>
           })}
         </div>
