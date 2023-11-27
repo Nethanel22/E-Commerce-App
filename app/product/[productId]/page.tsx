c7d3cf5f2c92a2d4473ac1cf6bc974f28f171a45
Nethanel <nethanel676@gmail.com>
2023-11-27 20:00:20 +0200

    ProductDetails completed

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,9 +1,23 @@
+import Container from "@/app/components/Container";
+import ProductDetails from "./ProductDetails";
+import { product } from "@/utils/product";
+import ListRating from "./ListRating";
 interface IPrams{
     productId:string
 }
 const Product = ({params}:{params:IPrams}) => {
     console.log(params)
-    return (  <div> Product Page</div>);
+    return ( 
+         <div className="p-8"> 
+            <Container>
+                <ProductDetails product={product}/>
+                <div className="flex flex-col mt-20 gap-4">
+                    <div >Add Rating</div>
+                    <ListRating product={product}/>
+                </div>
+            </Container>
+         </div>
+         );
 }
  
 export default Product;
