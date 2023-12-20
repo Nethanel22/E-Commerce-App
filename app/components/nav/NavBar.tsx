af18f7baefca14fa14ff6fb98dc0430631ad6018
Nethanel <nethanel676@gmail.com>
2023-12-20 15:02:21 +0200

    SearchBar completed

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -30,6 +30,7 @@ import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Categories from "./Categories";
+import SearchBar from "./SearchBar";
 const redressed=Redressed({subsets:['latin'],weight:["400"]})
 const NavBar = async() => {
     const currentUser=await getCurrentUser()
@@ -47,7 +48,7 @@ const NavBar = async() => {
 <Container>
     <div className="flex items-center justify-between gap-3 md:gap-0 ">
         <Link href='/' className={`${redressed.className} font-bold text-2xl `}> E-Shop</Link>
-        <div className="hidden md:block">Search</div>
+        <div className="hidden md:block"><SearchBar/></div>
         <div className="flex items-center gap-8 md:gap-12">
             <CartCount/>
             <UserMenu currentUser={currentUser}/>
